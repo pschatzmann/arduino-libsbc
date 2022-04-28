0dcb552eacb3b6544c4fe76e196f3ba878521172
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-04-28 08:58:14 +0200

    Arduino support for big and Liddle endinan

--- src/sbc/formats.h
+++ src/sbc/formats.h
@@ -24,12 +24,21 @@
 #include <byteswap.h>
 
 #ifdef ARDUINO 
+#if __BYTE_ORDER == __LITTLE_ENDIAN
 #define COMPOSE_ID(a,b,c,d)	((a) | ((b)<<8) | ((c)<<16) | ((d)<<24))
 #define LE_SHORT(v)		(v)
 #define LE_INT(v)		(v)
 #define BE_SHORT(v)		__builtin_bswap16(v)
 #define BE_INT(v)		__builtin_bswap32(v)
+#elif __BYTE_ORDER == __BIG_ENDIAN
+#define COMPOSE_ID(a,b,c,d)	((d) | ((c)<<8) | ((b)<<16) | ((a)<<24))
+#define LE_SHORT(v)		__builtin_bswap16(v)
+#define LE_INT(v)		__builtin_bswap32(v)
+#define BE_SHORT(v)		(v)
+#define BE_INT(v)		(v)
 #else
+#error "Wrong endian"
+#endif
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 #define COMPOSE_ID(a,b,c,d)	((a) | ((b)<<8) | ((c)<<16) | ((d)<<24))
 #define LE_SHORT(v)		(v)
